f499a1dad50a3b8bab3e987baeece906d44c0d85
menachem margolis <margolismendel5728@gmail.com>
2021-02-15 11:39:32 -0500

    basic backend strocture

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,13 @@
 class ImagesController < ApplicationController
+
+  def create
+    image = image.create(new_image_params)
+  end
+
+
+  private
+
+  def new_image_params
+    params.permit(:url, :vacation_id)
+  end
 end
--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -1,2 +1,13 @@
 class VacationsController < ApplicationController
+
+    def create
+        vacation = Vacation.create(new_vacation_params)
+    end
+    
+    
+      private
+    
+      def new_vacation_params
+        params.permit(:location, :user_id)
+      end
 end

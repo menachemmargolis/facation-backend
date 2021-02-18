14fb3a87c623887591d7a38fb300d611e582e4d8
shmuly11 <shmuly11@gmail.com>
2021-02-18 11:32:30 -0500

    companion sizes

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -7,7 +7,7 @@ class VacationsController < ApplicationController
 
     def create
         
-        vacation = Vacation.create(user_id: 1, location: new_vacation_params[:location])
+        vacation = Vacation.create(user_id: 1, companion_id: new_vacation_params[:companion], location: new_vacation_params[:location])
         new_vacation_params[:images].each{|image| vacation.images.create(url: image)}
         
         vacation.images.each{|image| image.composite_image}
@@ -17,7 +17,7 @@ class VacationsController < ApplicationController
     def locations 
       
       location = params[:location]
-      url = "https://api.unsplash.com/search/photos?client_id=#{Figaro.env.unsplash_api_key}&query=#{location}&page=#{params[:number]}&per_page=3"
+      url = "https://api.unsplash.com/search/photos?client_id=#{Figaro.env.unsplash_api_key}&query=#{location}&page=#{params[:number]}&per_page=3&orientation=landscape"
       response = RestClient.get(url)
       body = response.body
       parsed = JSON.parse(body)
@@ -29,6 +29,6 @@ class VacationsController < ApplicationController
       private
     
       def new_vacation_params
-        params.permit(:location, :user_id, :images => [])
+        params.permit(:location, :user_id, :companion, :images => [])
       end
 end
